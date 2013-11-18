13196cb80bb9048942f2cd79671faae0a1be7b2e
David Coss <david@davecoss.com>
2013-11-18 08:12:57 -0700

    Added BuildInfo to library.

---
+++ src/com/davecoss/java/BuildInfo.java
@@ -0,0 +1,53 @@
+package com.davecoss.java;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class BuildInfo {
+	private Properties info_data = null;
+
+	public BuildInfo() {
+		info_data = new Properties();
+		InputStream info_file = get_info_file("build.info");
+		if (info_file == null) {
+			info_data = null;
+		} else {
+			try {
+				info_data.load(info_file);
+			} catch (IOException e) {
+				info_data = null;
+			}
+		}
+	}
+	
+	public BuildInfo(@SuppressWarnings("rawtypes") Class c) {
+		info_data = new Properties();
+		InputStream info_file = get_info_file("build.info", c);
+		if (info_file == null) {
+			info_data = null;
+		} else {
+			try {
+				info_data.load(info_file);
+			} catch (IOException e) {
+				info_data = null;
+			}
+		}
+	}
+
+	public Properties get_build_properties() {
+		return info_data;
+	}
+
+	public String get_version() {
+		return info_data.getProperty("version");
+	}
+	
+	public InputStream get_info_file(String filename) {
+		 return get_info_file(filename, this.getClass());
+	}
+	
+	public InputStream get_info_file(String filename, @SuppressWarnings("rawtypes") Class c) {
+		return c.getResourceAsStream(filename);
+	}
+}
--- src/com/davecoss/java/Main.java
+++ src/com/davecoss/java/Main.java
@@ -2,6 +2,7 @@ package com.davecoss.java;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("This is a java library.");
+		BuildInfo bi = new BuildInfo();
+		System.out.println("This is a java library. Version: " + bi.get_version());
 	}
 }
