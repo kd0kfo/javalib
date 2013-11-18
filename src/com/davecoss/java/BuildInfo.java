56678790e6e477af2ebdac2cd1ac19ff6db8a5fa
David Coss <david@davecoss.com>
2013-11-18 08:20:23 -0700

    Removed default constructor from BuildInfo

--- src/com/davecoss/java/BuildInfo.java
+++ src/com/davecoss/java/BuildInfo.java
@@ -7,20 +7,6 @@ import java.util.Properties;
 public class BuildInfo {
 	private Properties info_data = null;
 
-	public BuildInfo() {
-		info_data = new Properties();
-		InputStream info_file = get_info_file("build.info");
-		if (info_file == null) {
-			info_data = null;
-		} else {
-			try {
-				info_data.load(info_file);
-			} catch (IOException e) {
-				info_data = null;
-			}
-		}
-	}
-	
 	public BuildInfo(@SuppressWarnings("rawtypes") Class c) {
 		info_data = new Properties();
 		InputStream info_file = get_info_file("build.info", c);
--- src/com/davecoss/java/Main.java
+++ src/com/davecoss/java/Main.java
@@ -2,7 +2,7 @@ package com.davecoss.java;
 
 public class Main {
 	public static void main(String[] args) {
-		BuildInfo bi = new BuildInfo();
+		BuildInfo bi = new BuildInfo(Main.class);
 		System.out.println("This is a java library. Version: " + bi.get_version());
 	}
 }
