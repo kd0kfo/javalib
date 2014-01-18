92868d8fce6043dcffde84d94780081bfc277920
David Coss <david@davecoss.com>
2014-01-18 13:28:36 -0600

    Updated unit test.

--- test/com/davecoss/java/Test.java
+++ test/com/davecoss/java/Test.java
@@ -2,7 +2,7 @@ package com.davecoss.java;
 
 public class Test {
 	
-	public static LogHandler L = new ConsoleLog("Unit Test");
+	public static Logger L = ConsoleLog.getInstance("Unit Test");
 
 	@org.junit.Test
 	public void testMe() {
