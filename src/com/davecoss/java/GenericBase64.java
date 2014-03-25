a56fffad979863ab3c08336b11bcd34072e45128
David Coss <kd0kfo@users.noreply.github.com>
2014-03-25 15:10:52 -0500

    Added decodeInputStream

--- src/com/davecoss/java/GenericBase64.java
+++ src/com/davecoss/java/GenericBase64.java
@@ -8,6 +8,7 @@ package com.davecoss.java;
  */
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 
@@ -15,5 +16,6 @@ public interface GenericBase64 {
 
 	public String encode(byte[] data) throws Exception;
 	public byte[] decode(String string) throws Exception;
-	public OutputStream encodeOutputStream (OutputStream baseStream) throws IOException;	
+	public OutputStream encodeOutputStream (OutputStream baseStream) throws IOException;
+	public InputStream decodeInputStream (InputStream baseStream) throws IOException;
 }
